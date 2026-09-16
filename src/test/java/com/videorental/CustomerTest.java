b304594a8731715b760dadca02527307545a4d07
Dusenswf <wkdduswn7338@naver.com>
2026-09-16 16:31:29 +0900

    refact: test case refactoring

--- src/test/java/com/videorental/CustomerTest.java
+++ src/test/java/com/videorental/CustomerTest.java
@@ -5,23 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CustomerTest {
+    public static final String NAME = "NAME_NOT_IMPORTANT";
+    public static final String TITLE = "TITLE_NOT_IMPORTANT";
+    Customer customer = new Customer(NAME);
 
     @Test
     public void returnNewCustomer() {
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
         assertThat(customer).isNotNull();
     }
 
     @Test
     public void statementForNoRental() {
-        // arrange
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
-
-        // act
-        String statement = customer.statement();
-
         // assert
-        assertThat(statement).isEqualTo("Rental Record for NAME_NOT_IMPORTANT\n"
+        assertThat(customer.statement()).isEqualTo("Rental Record for NAME_NOT_IMPORTANT\n"
                 + "Amount owed is 0.0\n"
                 + "You earned 0 frequent renter pointers");
     }
@@ -29,11 +25,7 @@ public class CustomerTest {
     @Test
     public void statementForRegularMovieRentalForLessThan3Days() {
         // arrange
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
-        Movie movie = new Movie("TITLE_NOT_IMPORTANT", Movie.REGULAR);
-        int daysRented = 2;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(2, Movie.REGULAR));
 
         // act
         String statement = customer.statement();
@@ -45,14 +37,16 @@ public class CustomerTest {
                 + "You earned 1 frequent renter pointers");
     }
 
+    private static Rental createRentalFor(int daysRented, int priceCode) {
+        Movie movie = new Movie(TITLE, priceCode);
+        Rental rental = new Rental(movie, daysRented);
+        return rental;
+    }
+
     @Test
     public void statementForRegularMovieRentalForMoreThan2Days() {
         // arrange
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
-        Movie movie = new Movie("TITLE_NOT_IMPORTANT", Movie.REGULAR);
-        int daysRented = 3;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(3, Movie.REGULAR));
 
         // act
         String statement = customer.statement();
@@ -67,11 +61,7 @@ public class CustomerTest {
     @Test
     public void statementForNewReleaseMovie() {
         // arrange
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
-        Movie movie = new Movie("TITLE_NOT_IMPORTANT", Movie.NEW_RELEASE);
-        int daysRented = 1;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(1, Movie.NEW_RELEASE));
 
         // act
         String statement = customer.statement();
@@ -86,11 +76,7 @@ public class CustomerTest {
     @Test
     public void statementForChildrensMovieRentalMoreThan3Days() {
         // arrange
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
-        Movie movie = new Movie("TITLE_NOT_IMPORTANT", Movie.CHILDRENS);
-        int daysRented = 4;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(4, Movie.CHILDRENS));
 
         // act
         String statement = customer.statement();
@@ -105,11 +91,7 @@ public class CustomerTest {
     @Test
     public void statementForChildrensMovieRentalLessThan4Days() {
         // arrange
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
-        Movie movie = new Movie("TITLE_NOT_IMPORTANT", Movie.CHILDRENS);
-        int daysRented = 3;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(3, Movie.CHILDRENS));
 
         // act
         String statement = customer.statement();
@@ -124,11 +106,7 @@ public class CustomerTest {
     @Test
     public void statementForNewReleaseMovieRentalMoreThan1Day() {
         // arrange
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
-        Movie movie = new Movie("TITLE_NOT_IMPORTANT", Movie.NEW_RELEASE);
-        int daysRented = 2;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(2, Movie.NEW_RELEASE));
 
         // act
         String statement = customer.statement();
@@ -142,10 +120,9 @@ public class CustomerTest {
 
     @Test
     public void statementForFewMovieRental() {
-        Customer customer = new Customer("NAME_NOT_IMPORTANT");
-        Movie regularMovie = new Movie("TITLE_NOT_IMPORTANT", Movie.REGULAR);
-        Movie newReleaseMovie = new Movie("TITLE_NOT_IMPORTANT", Movie.NEW_RELEASE);
-        Movie childrensMovie = new Movie("TITLE_NOT_IMPORTANT", Movie.CHILDRENS);
+        Movie regularMovie = new Movie(TITLE, Movie.REGULAR);
+        Movie newReleaseMovie = new Movie(TITLE, Movie.NEW_RELEASE);
+        Movie childrensMovie = new Movie(TITLE, Movie.CHILDRENS);
         customer.addRental(new Rental(regularMovie, 1));
         customer.addRental(new Rental(newReleaseMovie, 4));
         customer.addRental(new Rental(childrensMovie, 4));
@@ -157,4 +134,5 @@ public class CustomerTest {
                 + "Amount owed is 17.0\n"
                 + "You earned 4 frequent renter pointers");
     }
+
 }
